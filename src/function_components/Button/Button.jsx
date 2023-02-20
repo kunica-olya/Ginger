f30d970107960a6fc6bd6ff327f1c8301c77b124
Kunica Olya <olyavolk49@gmail.com>
2023-02-21 01:29:38 +0200

    added import icon for function component button

--- src/function_components/Button/Button.jsx
+++ src/function_components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import styles from "../Button/Button.module.scss";
-import Icon from '../../assets/svg/Vector.svg';
+import {ReactComponent as Icon} from "../../assets/svg/Vector.svg";
 
 const Button = () => {
     return (
         <a href='https://instagram.com/ginger.konditori' className={styles['button-subscribe']}>
-            <img className={styles['button-icon']} src={Icon} alt="icon"/>
+            <Icon/>
             <span className={styles['button-text']}>Subscribe</span>
         </a>
     )
