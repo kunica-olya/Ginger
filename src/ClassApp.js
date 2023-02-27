f3c2fd05f53ac8492116685aebca3b9d7149b8a6
Kunica Olya <olyavolk49@gmail.com>
2023-02-28 00:13:22 +0200

    added props for components

--- src/ClassApp.js
+++ src/ClassApp.js
@@ -4,12 +4,34 @@ import Main from './class_components/Main/Main';
 import Footer from './class_components/Footer/Footer';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            config: {},
+        }
+    }
+
+    componentDidMount() {
+        fetch('/app_config.json')
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    config: data
+                });
+                console.log('data', data)
+                console.log('this.state', this.state)
+            })
+            .catch(error => console.error('Error fetching app_config_json', error));
+    }
+
     render() {
+        const {config} = this.state
+
         return (
             <>
-                <Header/>
-                <Main/>
-                <Footer/>
+                <Header config={config}/>
+                <Main config={config}/>
+                <Footer config={config}/>
             </>
         )
     }
