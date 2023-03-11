dd7550820f72508f66375d26c25d8189d01f0cf8
Kunica Olya <olyavolk49@gmail.com>
2023-03-11 16:08:34 +0200

    added conditions for showing different icons of button

--- src/class_components/Button/ButtonView.jsx
+++ src/class_components/Button/ButtonView.jsx
@@ -1,5 +1,6 @@
 import {Component} from 'react';
-import {ReactComponent as Icon} from "../../assets/svg/bag.svg";
+import {ReactComponent as IconBag} from "../../assets/svg/bag.svg";
+import {ReactComponent as IconInstagram} from "../../assets/svg/instagram.svg";
 import styles from './Button.module.scss';
 
 export default class ButtonView extends Component {
@@ -9,7 +10,8 @@ export default class ButtonView extends Component {
 
         return (
             <button onClick={click} className={styles['button-buy']}>
-                <Icon/>
+                {text === "Subscribe" && <IconInstagram/>}
+                {text === "Buy" && <IconBag/>}
                 <span className={styles['button-text']}>{text}</span>
             </button>
         )
