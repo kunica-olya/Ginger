480e1dab01bfff0397277d6e85bd6f93ada826bd
Kunica Olya <olyavolk49@gmail.com>
2023-03-20 02:58:12 +0200

    [LSS-3] fixed error

--- src/pages/OrderTable/OrderTableView.jsx
+++ src/pages/OrderTable/OrderTableView.jsx
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import styles from "./OrderTable.module.scss";
 import {Modal} from "./Modal/Modal";
+import {ButtonRemoveView} from "./ButtonRemove/ButtonRemoveView";
 
 
 export class OrderTableView extends Component {
