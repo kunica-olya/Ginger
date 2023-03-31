b4637bfd98466a2e172b8bb86387c35171b374b2
Kunica Olya <olyavolk49@gmail.com>
2023-04-01 00:15:40 +0300

    added component Todo

---
+++ src/pages/Todo/Todo.jsx
@@ -0,0 +1,50 @@
+import {Component} from "react";
+import {TodoView} from "./TodoView";
+
+export class Todo extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos: [],
+            userValue: '',
+            isCreatedTodo: false,
+        }
+    }
+
+
+    handlerOnChange = (e) => {
+
+        this.setState({
+            userValue: e.target.value
+        })
+    }
+
+    handlerAddTask = () => {
+
+        const {userValue} = this.state;
+
+        this.setState(({todos}) => ({
+            todos: [...todos, userValue],
+            isCreatedTodo: true,
+            userValue: ''
+        }))
+
+    }
+
+
+    render() {
+        const {isCreatedTodo, userValue, todos} = this.state;
+        return (
+            <>
+                <TodoView todos={todos}
+                          changeInput={this.handlerOnChange}
+                          isCreatedTodo={isCreatedTodo}
+                          userValue={userValue}
+                          handlerAddTask={this.handlerAddTask}
+                />
+            </>
+        )
+    }
+}
