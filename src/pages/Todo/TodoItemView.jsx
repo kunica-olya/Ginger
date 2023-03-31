6f64450ce005d14a87bc5f133f7910bcd1c51140
Kunica Olya <olyavolk49@gmail.com>
2023-04-01 00:21:00 +0300

    added component TodoItemView

---
+++ src/pages/Todo/TodoItemView.jsx
@@ -0,0 +1,20 @@
+import {Component} from "react";
+import styles from "./TodoItemView.module.scss"
+
+export class TodoItemView extends Component {
+
+    render() {
+
+        const {todos} = this.props;
+
+        return (
+            <div className={styles['todo-items']}>
+                {
+                    todos.map((todo, index) => (
+                        <div className={styles['todo-item']} key={index}>{todo}</div>
+                    ))
+                }
+            </div>
+        )
+    }
+}
