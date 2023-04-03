b286328beff6110aced669a143197b258dcb353e
Kunica Olya <olyavolk49@gmail.com>
2023-04-03 19:10:51 +0300

    refactor dragAndDrop

--- src/pages/Todo/TodoItemView.jsx
+++ src/pages/Todo/TodoItemView.jsx
@@ -1,86 +1,22 @@
-import {Component} from "react";
-import styles from "./TodoItemView.module.scss"
+import React, {Component} from "react";
+import styles from "./TodoItemView.module.scss";
 
 export class TodoItemView extends Component {
 
-    componentDidMount() {
-        let items = document.querySelectorAll(`.${styles['todo-items']}`)
-        items.forEach((item) => {
-            item.addEventListener('dragstart', this.handlerDragStart);
-            item.addEventListener('dragenter', this.handlerDragEnter);
-            item.addEventListener('dragleave', this.handlerDragLeave);
-            item.addEventListener('dragover', this.handlerDragOver);
-            item.addEventListener('dragend', this.handlerDragEnd);
-            item.addEventListener('drop', this.handlerDrop);
-        })
-    }
-
-
-    componentWillUnmount() {
-        let items = document.querySelectorAll(`.${styles['todo-items']}`)
-        items.forEach((item) => {
-            item.removeEventListener('dragstart', this.handlerDragStart);
-            item.removeEventListener('dragenter', this.handlerDragEnter);
-            item.removeEventListener('dragleave', this.handlerDragLeave);
-            item.removeEventListener('dragover', this.handlerDragOver);
-            item.removeEventListener('dragend', this.handlerDragEnd);
-            item.removeEventListener('drop', this.handlerDrop);
-        })
-    }
-
-
-    handlerDragStart = (e) => {
-        e.target.style.opacity = '0.4';
-        this.dragElem = e.target;
-
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/html', e.target.innerHTML);
-    }
-
-    handlerDragEnd = (e) => {
-        e.target.style.opacity = '1';
-        let items = document.querySelectorAll(`.${styles['todo-items']}`)
-        items.forEach((item) => {
-            item.classList.remove('over');
-        })
-    }
-
-    handlerDragOver = (e) => {
-        if (e.preventDefault) {
-            e.preventDefault();
-        }
-        return false;
-    }
-
-    handlerDrop = (e) => {
-        e.stopPropagation();
-        if (this.dragElem !== e.target) {
-            this.dragElem.innerHTML = e.target.innerHTML;
-            e.target.innerHTML = e.dataTransfer.getData('text/html');
-        }
-
-        return false;
-    }
-
-
-    handlerDragEnter = (e) => {
-        e.target.classList.add('over');
-    }
-
-    handlerDragLeave = (e) => {
-        e.target.classList.remove('over');
-    }
-
-
     render() {
 
-        const {todos} = this.props;
+        const {todos, refs} = this.props;
 
         return (
+
             <div className={styles['todo-items']}>
                 {
                     todos.map((todo, index) => (
-                        <div draggable={'true'} className={styles['todo-item']} key={index}>{todo}</div>
+                        <div draggable={'true'} ref={refs[index]} className={styles['todo-item']}
+                             key={index}
+                        >
+                            {todo}
+                        </div>
                     ))
                 }
             </div>
