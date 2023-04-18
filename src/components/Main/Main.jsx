333374c47b30e28188b569609c9f809624029bca
Kunica Olya <olyavolk49@gmail.com>
2023-04-18 16:06:11 +0300

    add spinner

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import {MainView} from "./MainView";
 import {withLayout} from "../HOC/withLayout";
 import {useState, useEffect} from "react";
+import {Oval} from "react-loader-spinner";
 
 const Main = () => {
 
@@ -12,18 +13,35 @@ const Main = () => {
         fetch('/cardsData.json')
             .then(response => response.json())
             .then(data => {
-                setCards(data);
-                setDataIsLoaded(true);
+                setTimeout(() => {
+                    setCards(data);
+                    setDataIsLoaded(true);
+                }, 3000)
+
             })
             .catch(error => console.error('Error fetching data_json', error));
     }, []);
 
 
     return (
-        <>
-            {dataIsLoaded && <MainView data={cards}/>}
-            {!dataIsLoaded && <div>Loading</div>}
-        </>
+        <div>
+            {dataIsLoaded ? (
+                <MainView data={cards}/>
+            ) : (
+                <Oval
+                    height={50}
+                    width={50}
+                    color="#4fa94d"
+                    wrapperStyle={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+                    wrapperClass=""
+                    visible={true}
+                    ariaLabel='oval-loading'
+                    secondaryColor="#4fa94d"
+                    strokeWidth={2}
+                    strokeWidthSecondary={2}
+                />
+            )}
+        </div>
     )
 }
 
