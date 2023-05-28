8a34f87d30d5030e7b5b7b83411e374cbab2a548
Kunica Olya <olyavolk49@gmail.com>
2023-05-28 20:32:58 +0300

    add axios for request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Main from './layout/Main/Main';
 import OrderTable from './pages/OrderTable/OrderTable';
 import Todo from './pages/Todo/Todo';
-
 import './i18n';
 
 export default function App() {
   const [config, setConfig] = useState({});
 
+  const getConfig = async () => {
+    const {
+      data,
+      error
+    } = await axios.get('/app_config.json');
+    setConfig(data);
+    if (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    fetch('/app_config.json')
-      .then((response) => response.json())
-      .then((data) => {
-        setConfig(data);
-      })
-    // eslint-disable-next-line no-console
-      .catch((error) => console.log('Error fetching data_json', error));
+    getConfig();
   }, []);
 
   return (
